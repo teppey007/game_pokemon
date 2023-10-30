bb3e70ad9b926663f6f376c70821a961b1c851c7
teppey007 <teppei.aoki@nmsc.co.jp>
2023-10-30 12:56:51 +0900

    ボールゲーム追加

--- js/main.js
+++ js/main.js
@@ -39,3 +39,21 @@
 
 }   
 
+
+
+$(function () {
+  setInterval(()=>{
+      $('#ball').css('left',Math.random()*450 +'px');
+      $('#ball').css('top',Math.random()* 450 +'px');
+  },2000)    
+
+  let point = 0;
+
+  $('#ball').on('click',()=>{
+      point += 100;
+      $('#point').html(point);
+  })    
+
+
+
+})
